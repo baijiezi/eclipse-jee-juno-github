8824edb6e8e830e61f612fa6249cd66c1578bf0f
BaiJiezi <shaogc@968309.com>
2014-06-03 10:03:49 +0800

    c

--- myworkspace/MyJavaPrj/src/foundation/gc/OutOfMemoryTest.java
+++ myworkspace/MyJavaPrj/src/foundation/gc/OutOfMemoryTest.java
@@ -3,7 +3,21 @@ package foundation.gc;
 import java.util.ArrayList;
 import java.util.List;
 
-//OutOfMemoryError   默认内存64M
+/**
+Exception in thread "main" java.lang.OutOfMemoryError: Java heap space
+
+Heap size 设置
+JVM堆的设置是指java程序运行过程中JVM可以调配使用的内存空间的设置.JVM在启动的时候会自动设置Heap size的值，
+其初始空间(即-Xms)是物理内存的1/64，最大空间(-Xmx)是物理内存的1/4。可以利用JVM提供的-Xmn -Xms -Xmx等选项可
+进行设置，一般最大空间设置不应大于物理内存80%.
+ 
+ -Xms  JVM初始化堆的大小
+ -Xmx  JVM堆的最大值
+ 
+例如启动jar包时这样设置： 
+java -jar -Xms64m -Xmx512m xxxx.jar
+ 
+ **/
 public class OutOfMemoryTest 
 {
 	public static void main(String[] args)
@@ -22,14 +36,16 @@ public class OutOfMemoryTest
 			{
 				list.add(new byte[1024]);
 			}
-			System.out.print(i%1024 + " M");
+			System.out.print(i + " M");
 			currentTime = System.currentTimeMillis();
-			System.out.print("    " + (currentTime-lastTime) + " ms\n");
+			System.out.print("    " + (currentTime-lastTime) + " ms");
 			lastTime = currentTime;
 			
+			System.out.print(Runtime.getRuntime().totalMemory() + " " + Runtime.getRuntime().freeMemory() + " " + Runtime.getRuntime().maxMemory());
+			System.out.println();
 			try 
 			{
-				Thread.sleep(1000);
+				Thread.sleep(10);
 			} 
 			catch (InterruptedException e) 
 			{
@@ -39,3 +55,6 @@ public class OutOfMemoryTest
 		}
 	}
 }
+
+
+
